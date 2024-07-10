b706ca04b52e132104c89b1429be1cc544f831c8
Rubsee <r.u.s.i.k.99@mail.ru>
2024-07-10 23:59:24 +0300

    Правки интерактивных элементов
    Все ровно не появляется модальное окно при нажатии на .product-list__button, скорее всего не правильно что-то сделал в modal.js

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,18 +1,17 @@
-const modalOpen = document.querySelectorAll('.product-list__button');
+const modalOpened = document.querySelectorAll('.product-list__button');
 const modal = document.querySelector('.slick-notification');
 const modalClosed = document.querySelector('.slick-notification__close')
-const modalContinue = document.querySelector('.slick-notification__accept');
+const modalContinue = document.querySelector('.slick-notification__accept.black-button');
 
-modalOpen.forEach((button) => {
+modalOpened.forEach((button) => {
     button.addEventListener('click', () => {
         modal.classList.add('slick-notification--showed');
-        modalClosed.addEventListener('click', closeModal);
-        modalContinue.addEventListener('click', closeModal);
     });
 });
 
 const closeModal = () => {
     modal.classList.remove('slick-notification--showed');
-    modalClosed.remdoveEventListener('click', closeModal);
-    modalContinue.removeEventListener('click', closeModal);
 };
+
+modalClosed.addEventListener('click', closeModal);
+modalContinue.addEventListener('click', closeModal);
