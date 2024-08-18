45b139068606155789eed927913dc9735872b880
Rubsee <r.u.s.i.k.99@mail.ru>
2024-08-18 03:39:16 +0300

    Работа над ошибками

--- source/js/sliders.js
+++ source/js/sliders.js
@@ -6,15 +6,15 @@ const swiper = new Swiper('.banners__swiper', {
     spaceBetween: 30,
     // If we need pagination
     pagination: {
-        el: '.banners__pagination-list',
-        bulletActiveClass: 'banners__pagination-item--active',
-        bulletClass: 'banners__pagination-item',
+        el: '.pagination--banners',
+        bulletActiveClass: 'pagination__item--active',
+        bulletClass: 'pagination__item',
     },
 
     // Navigation arrows
     navigation: {
-        nextEl: '.banners__arrow--right',
-        prevEl: '.banners__arrow--left',
+        nextEl: '.arrow__banners--right',
+        prevEl: '.arrow__banners--left',
     },
 });
 
@@ -38,14 +38,14 @@ const swiperPopular = new Swiper('.most-popular__swiper', {
     },
     // If we need pagination
     pagination: {
-        el: '.most-popular__pagination-list',
-        bulletActiveClass: 'most-popular__pagination-item--active',
-        bulletClass: 'most-popular__pagination-item',
+        el: '.pagination--most-popular',
+        bulletActiveClass: 'pagination__item--active',
+        bulletClass: 'pagination__item',
     },
 
     // Navigation arrows
     navigation: {
-        nextEl: '.most-popular__arrow--right',
-        prevEl: '.most-popular__arrow--left',
+        nextEl: '.arrow__most-popular--right',
+        prevEl: '.arrow__most-popular--left',
     },
 });
