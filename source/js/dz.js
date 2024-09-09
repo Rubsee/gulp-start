432ee20b2be027376102084586555676c53d5c5e
Rubsee <r.u.s.i.k.99@mail.ru>
2024-09-09 12:46:07 +0300

    Исправил реализацию

--- source/js/dz.js
+++ source/js/dz.js
@@ -36,28 +36,75 @@ const flats = [
 ];
 
 
-const foriverFlats = flats
-  .filter(flat => flat.project.name === 'Foriver')    //On
-  .map(flat => ({                                     //On²
-    floor: flat.floor,
-    rooms: flat.rooms
-  }));
+//ВАРИАНТ 3(после подсказки)
+const addedProjects = [];
 
-const foriver = [{ id: 1, name: 'Foriver', flats: [...foriverFlats] }]
+const projectAll = flats.map(flat => {
+  const { project, ...flatWithoutProject } = flat;
+
+  let existingProject = addedProjects.find(proj => proj.id === project.id);
+
+  if (!existingProject) {
+    const newProject = {
+      name: flat.project.name,
+      id: flat.project.id,
+      flats: [flatWithoutProject],
+    };
+    addedProjects.push(newProject);
+  } else {
+    existingProject.flats.push(flatWithoutProject);
+  }
+});
+
+console.log(addedProjects)
 
 
-const riverskyFlats = flats
-  .filter(flat => flat.project.name === 'Riversky')   //On
-  .map(flat => ({                                     //On²
-    floor: flat.floor,
-    rooms: flat.rooms
-  }));
-
-const riversky = [{ id: 2, name: 'Riversky', flats: [...riverskyFlats] }]
 
 
-const projects = [...foriver, ...riversky]
-console.log(projects)
+// ВАРИАНТ 2 (с нейронкой)
+// const uniqueProjects = Array.from(new Set(flats.map(flat => flat.project.id)))
+//   .map(id => {
+//     const { name } = flats.find(flat => flat.project.id === id).project;
+//     return {
+//       id,
+//       name,
+//       flats: flats
+//         .filter(flat => flat.project.id === id)
+//         .map(flat => ({
+//           floor: flat.floor,
+//           rooms: flat.rooms,
+//         }))
+//     };
+//   });
+
+// console.log(uniqueProjects);
+
+
+
+
+// ВАРИАНТ 1
+// const foriverFlats = flats
+//   .filter(flat => flat.project.name === 'Foriver')    //On
+//   .map(flat => ({                                     //On²
+//     floor: flat.floor,
+//     rooms: flat.rooms
+//   }));
+
+// const foriver = [{ id: 1, name: 'Foriver', flats: [...foriverFlats] }]
+
+
+// const riverskyFlats = flats
+//   .filter(flat => flat.project.name === 'Riversky')   //On
+//   .map(flat => ({                                     //On²
+//     floor: flat.floor,
+//     rooms: flat.rooms
+//   }));
+
+// const riversky = [{ id: 2, name: 'Riversky', flats: [...riverskyFlats] }]
+
+
+// const projects = [...foriver, ...riversky]
+// console.log(projects)
 
 // чего нужно добиться
 // const projects = [
